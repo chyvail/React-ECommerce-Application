7c1e330258642288074c83a6fb96f470a8149593
Joshua Chivile <joshuachivile@gmail.com>
2022-06-23 09:52:25 +0300

    made changes to navbar

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -3,22 +3,26 @@ import Link from 'next/link';
 
 import { urlFor, urlfor } from '../lib/client';
 
-const HeroBanner = ({ HeroBanner }) => {
+// destructuring 
+
+const HeroBanner = ({ HeroBanner : {
+  smallText, midText, largeText1, image, buttonText, desc
+}}) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>{ HeroBanner.smallText }</p>
-        <h3>{ HeroBanner.midText } </h3>
-        <h1>{ HeroBanner.largeText1 } </h1>
-        <img src={urlFor(HeroBanner.image)} alt='headphones' className='hero-banner-image'/>
+        <p className='beats-solo'>{ smallText }</p>
+        <h3>{ midText } </h3>
+        <h1>{ largeText1 } </h1>
+        <img src={urlFor(image)} alt='headphones' className='hero-banner-image'/>
       </div>
       <div>
         <Link href={"/product/${HeroBanner.product}"} >
-          <button type='button'>{HeroBanner.buttonText}</button>
+          <button type='button'>{buttonText}</button>
         </Link>
         <div className='desc'>
           <h5>Description</h5>
-          <p>{HeroBanner.desc}</p>
+          <p>{desc}</p>
         </div>
       </div>
     </div>
