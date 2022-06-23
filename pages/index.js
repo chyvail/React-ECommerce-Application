b55bf8030154ba6f56db002ae4cddb83137f6b53
Joshua Chivile <joshuachivile@gmail.com>
2022-06-23 08:56:05 +0300

    made changes to products props

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,42 @@
 import React from 'react'
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner} from '../components';
 
-const index = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
+
+      <HeroBanner HeroBanner={bannerData.length && bannerData[0]}/>
       <div className='products-heading'>
           <h2>Best Selling Products</h2>
           <p>Speakers of many variations</p>
       </div>
 
       <div className='products-container'>
-        {['product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => <Product key =
+        {product._id} product={product} />)}
       </div>
 
-      Footer
+      <FooterBanner footerBanner={bannerData.length && bannerData[0]}/>
 
     </>
   )
 }
 
-export default index
+//to get data from the server side we use getServerSideProps in nextjs
+export const getServerSideProps = async () => {
+  
+  //get product data
+  const query = '*[_type == "product" ]';
+  const products = await client.fetch(query);
+
+  //get banner data
+  const bannerQuery = '*[_type == "banner" ]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return{
+    props: { products, bannerData}
+  }
+}
+
+export default Home;
